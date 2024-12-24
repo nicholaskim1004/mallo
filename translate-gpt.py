0878802399be348d9fe08a8c14a564b85c400d39
Nick Kim <nicholaskim.1010@gmail.com>
2024-12-24 13:25:49 -0600

    - added strip when reading from api text file - added a try and except to output errors - changed to import openai and .ChatCompletion

--- translate-gpt.py
+++ translate-gpt.py
@@ -1,34 +1,32 @@
-from openai import OpenAI
-
-#create sep file that'll store api key and add to gitignore
-#which api_key will read from
+import openai 
 
 with open("key.txt","r") as file:
-    key = file.read()
+    key = file.read().strip()
 
-client = OpenAI(
-  api_key=key
-)
+openai.api_key = key
 
 def chat_w_gpt(prompt):
-    response = client.chat.completions.create(
-        model = "gpt-4o-mini",
-        store = True,
-        messages = [{"role": "system", "content": "You are a Korean teacher that'll have a conversation on given topic at a given speaking level. Please go one sentence at a time."} ,
-                    {"role": "user", "content": prompt}]
-    )
+    try:
+        response = openai.ChatCompletion.create(
+            model = "gpt-4",
+            store = True,
+            messages = [{"role": "system", "content": "You are a Korean teacher that'll have a conversation on given topic at a given speaking level. Please go one sentence at a time."} ,
+                        {"role": "user", "content": prompt}]
+        )
     
-    return response.choices[0].message.content.strip()
+        return(response.choices[0].message.content.strip())
+    except Exception as e:
+        return(f"There was an error: {e}")
 
 
 
 if __name__ == "__main__":
     while True:
+        print("Enter level of fluency and what type of conversation you'd like to have")
         user_input = input("User: ")
         if user_input.lower() in ["quit","exit","bye"]:
             break
         
-        print("Enter level of fluency and what type of conversation you'd like to have")
         response = chat_w_gpt(user_input)
         print(f"GPT: {response}")
         
