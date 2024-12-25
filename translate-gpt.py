2bbf6d56e7f26df6c3245d2d0e12c1a8b0e2c946
Nick Kim <nicholaskim.1010@gmail.com>
2024-12-24 23:01:24 -0600

    - hopefully implemented whisper model to transcribe spoken audio file to korean which will feed into the conversation with gpt

--- translate-gpt.py
+++ translate-gpt.py
@@ -1,4 +1,7 @@
 from openai import OpenAI
+import pyaudio
+import whisper
+import numpy as np
 
 with open("key.txt","r") as file:
     key = file.read().strip()
@@ -10,7 +13,7 @@ client = OpenAI(
 def chat_w_gpt(prompt):
     try:
         response = client.chat.completions.create(
-            model = "gpt-4",
+            model = "gpt-4o-mini",
             store = True,
             messages = [{"role": "system", "content": "You are a Korean teacher that'll have a conversation on given topic at a given speaking level. Please go one sentence at a time."} ,
                         {"role": "user", "content": prompt}]
@@ -22,14 +25,36 @@ def chat_w_gpt(prompt):
 
 first_run = True
 
+#loading whisper model
+whisper_mod = whisper.load_model("base")
+
+#initalizing parameters: 
+#RATE (num of samples collected per sec), CHUNK (num of frames in buffer)
+RATE = 16000
+CHUNK = 1024
+
+p = pyaudio.PyAudio
+
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True, frames_per_buffer=CHUNK)
+
 if __name__ == "__main__":
     while True:
         if first_run:
              print("Enter level of fluency and what type of conversation you'd like to have")
              first_run = False
-             
-        user_input = input("User: ")
+        
+        audio_data = stream.read(CHUNK, exception_on_overflow=False)
+        
+        #normalizing the audio file by 32768 as 16 bit can range from -32,767 to 32,767
+        audio_np = np.frombuffer(audio_data, dtype=np.int16).astype(np.float32) / 32768.0
+        
+        res = whisper_mod.transcribe(audio_np, language="ko")
+     
+        user_input = input(f"User: {res['text']}")
         if user_input.lower() in ["quit","exit","bye"]:
+            stream.stop_stream()
+            stream.close()
+            p.terminate()
             break
         
         response = chat_w_gpt(user_input)
